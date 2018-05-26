a4318157368b6477ae14fdd197c6ff496636e7c4
ronald8192 <u23805@gmail.com>
2018-05-26 15:30:41 +0800

    init

---
+++ src/main/java/com/ronald8192/djifixgui/App.java
@@ -0,0 +1,15 @@
+package com.ronald8192.djifixgui;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		Logger log = LoggerFactory.getLogger(App.class);
+
+	}
+}
